82a751a7f4fd8fc413a5c2d2a06329766ca4c390
malavikarajeevan <malavikarajeevan85@gmail.com>
2024-02-26 08:13:46 +0530

    new file added

--- exam/userproj/userapp/views.py
+++ exam/userproj/userapp/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+
 def Home(request):
-    
-    return(render,'index.html')
+    return render(request, 'index.html')
+
 # Create your views here.
